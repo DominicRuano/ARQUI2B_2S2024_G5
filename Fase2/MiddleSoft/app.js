d5bf794d00aaef1f8d0e55babc5dbd47669c40e4
200413657 <reningesis@gmail.com>
2024-09-05 23:47:09 -0600

    Se cambia configuración en app.js
    Se agregan las credenciales para conectarse a la base de datos remota

--- Fase2/MiddleSoft/app.js
+++ Fase2/MiddleSoft/app.js
@@ -8,13 +8,22 @@ const app = express();
 //configurar body-parser para procesar datos
 app.use(bodyParser.json());
 
-//Conf conexión
-const connection = mysql.createConnection({
+//Conf conexión local
+/*const connection = mysql.createConnection({
     host:'localhost',
     port:3306,
     user:'root',
     password:'$$Ace123',
     database:'ace2_p1_g5'
+});*/
+
+//conexión a base de datos en cloud
+const connection = mysql.createConnection({
+    host:'accesovehicular.cneoqseiqzhs.us-east-1.rds.amazonaws.com',
+    user:'admin',
+    password:'KSh95:Nt2b(j4cxW',
+    database:'accesovehicular'
+
 });
 //Abriendo conexión
 connection.connect((err)=>{
@@ -26,7 +35,7 @@ connection.connect((err)=>{
 });
 
 //Crea ruta POST
-app.post('/agregar',(req,res)=>{
+/*app.post('/agregar',(req,res)=>{
     const{device_src, val} = req.body;//extrae los datos de la petición
 
     const sql = 'INSERT INTO device_data(device_src, val) values(?,?)';
@@ -38,6 +47,57 @@ app.post('/agregar',(req,res)=>{
         res.status(200).send('Datos insertados correctamente, ID: '+results.insertId);
     });
 
+});*/
+
+//Post al sp 
+app.post('/agregar',(req,res)=>{
+    const data = req.body;
+    let completedQueries = 0;  // Contador para saber cuándo se han completado todas las consultas
+    let errors = [];  // Para almacenar posibles errores
+
+    data.forEach(item => {
+        const { device_src, val } = item;
+
+        // Llamada al stored procedure
+        const query = 'CALL insertarMedicion(?, ?, NULL, NULL)';
+        connection.query(query, [device_src, val], (err, result) => {
+            completedQueries++;
+
+            if (err) {
+                console.error('Error executing procedure:', err);
+                errors.push(err);
+            }
+
+            // Solo enviamos la respuesta cuando todas las consultas han finalizado
+            if (completedQueries === data.length) {
+                if (errors.length > 0) {
+                    // Si hay errores, se responde con un código de error y los errores
+                    res.status(500).send({ message: 'Error executing some queries', errors });
+                } else {
+                    // Si no hay errores, se responde con éxito
+                    res.status(200).send('Data inserted successfully');
+                }
+            }
+        });
+    });
+
+});
+
+//lectura
+app.get('/datos',(req,res)=>{
+    const { startDate, endDate } = req.query;
+
+    // Llamada al stored procedure
+    const query = `CALL obtenerMediciones(?, ?)`;
+    connection.query(query, [startDate, endDate], (err, results) => {
+        if (err) {
+            console.error('Error executing procedure:', err);
+            res.status(500).send('Error executing procedure');
+            return;
+        }
+
+        res.status(200).json(results[0]); // Los resultados se devuelven en el primer elemento del array
+    });
 });
 
 //Iniciando el servidor
